1e3601465e25b6fc89b622c402446772d1f37f25
Tejas20-03 <tejaschhabra.official@gmail.com>
2024-05-23 14:46:02 +0530

    Added Clerk Authentication

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex">
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Manage Your Expenses
+            <strong className="font-extrabold text-primary sm:block">
+              {" "}
+              Control Your Spends{" "}
+            </strong>
+          </h1>
+
+          <p className="mt-4 sm:text-xl/relaxed">
+            Start Monitoring your spends and save penny for future
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <Link
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              href="/sign-in"
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
